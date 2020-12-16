7da97eecc64b38f4501e6db28b514bdea5d8c67c
keelim <kimh00335@gmail.com>
2020-12-16 15:11:50 +0900

    Initial commit

---
+++ app/src/main/java/com/keelim/coronakorea/ui/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.keelim.coronakorea.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.keelim.coronakorea.R
+import com.keelim.coronakorea.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        initBinding()
+        initNavigation()
+    }
+
+    private fun initNavigation() {
+        val navController = findNavController(R.id.main_nav_host)
+        binding.mainBottomNavigation.setupWithNavController(navController)
+        // 위와 같은 2번째 방법
+//        NavigationUI.setupWithNavController(
+//            main_bottom_navigation,
+//            findNavController(R.id.main_nav_host)
+//        )
+    }
+
+    private fun initBinding() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.lifecycleOwner = this
+    }
+
+    companion object {
+        const val TAG = "MainActivityTag"
+    }
+}
